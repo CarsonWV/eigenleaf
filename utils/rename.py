93d3b140b44c6f3e0b4828b27c40be0cbc6193ea
CWeaver <carsonweaver@outlook.com>
2022-02-22 16:18:47 -0500

    Added image overlay support

--- utils/rename.py
+++ utils/rename.py
@@ -1,3 +1,11 @@
+# 2022-02-22 rename.py
+# Carson Weaver
+# GUI for rotating test set photos.
+
+# Many of the leaf images for this project are roatated at odd angles.
+# This complicates the PCA analysis beyond the scope of the project.
+# This tool allows users to quickly rotate their test images for for more efficient training.
+
 from tkinter import *
 from PIL import ImageTk, Image
 from time import sleep
@@ -7,10 +15,9 @@ from itertools import cycle
 class MainApplication:
 	def __init__(self, root):
 		self.root = root
-		root.title("Title Scanner")
+		root.title("Photo Rotator")
 
-		self.width, self.height = 500, 500 #root.wibnfo_screenwidth(), root.winfo_screenheight()
-		self.resize_tuple = (400, 400)
+		self.width, self.height = 650, int(445)
 
 		self.canv = Canvas(self.root, width=self.width, height=self.height, bg='black')
 		self.canv.grid(row=0, column=0)
@@ -21,50 +28,58 @@ class MainApplication:
 		self.root.bind('<Return>', self.ENTER_event)
 		self.done = False
 
-		self.get_photo_paths()
-		self.display_photo()
-		# Allow user to type a new name for the photo.
+		self.init_photo_viewer()
+		self.next_photo_viewer()
 
-		# Notify the User when they've renamed every photo.	
-
-	def get_photo_paths(self): # Returns a list of photos you can cycle through.
+	def init_photo_viewer(self): # Returns a list of photos you can cycle through.
 		photo_list = glob('../images/*.jpg')
 		self.initial = photo_list[0]
 		self.photo_cycle = cycle(_ for _ in photo_list)
 
-	def display_photo(self):
+	def next_photo_viewer(self):
 		current_photo = next(self.photo_cycle)
+		
+		# Check if you've gone through everything.
 		if current_photo == self.initial:
 			if not self.done:
 				self.done = True
 			else:
 				self.done_window()
-		print(current_photo)
-		self.load_next_image(current_photo)
 
-	def load_next_image(self, img_path): # So you can see the image you're renaming.
-		photo = Image.open(img_path)
-		#img_width, img_height = photo.size
-		resized_photo = photo.resize(self.resize_tuple, Image.ANTIALIAS)
+		# Load image to Tkinter interface.
+		photo = Image.open(current_photo)
+		resized_photo = photo.resize((200,200), Image.ANTIALIAS)
+
+		# TODO: Overlay protractor on image.
+		chart = Image.open('degree_chart.jpg')
+		resized_chart = chart.resize((int(1.5*450),450), Image.ANTIALIAS)
+
+		resized_chart.paste(resized_photo, (230, 140))
+		# Convert the Image object into a TkPhoto object
+		#tkimage = ImageTk.PhotoImage(imageHead)
+
+		# panel1 = Label(self.root, image=tkimage)
+		# panel1.grid(row=0, column=2, sticky=E)
+
 		global img
-		img = ImageTk.PhotoImage(resized_photo)  # PIL solution
-		self.canv.create_image(20, 20, anchor=NW, image=img)
+		img = ImageTk.PhotoImage(resized_chart)  # PIL solution
+		self.canv.create_image(0, 0, anchor=NW, image=img)
 
 	def done_window(self):
 		newWindow = Toplevel(self.root)
 		newWindow.title("New Window")
 		newWindow.geometry("200x200")
-		Label(newWindow, text="This is a new window").pack()
+		Label(newWindow, text="All photos have been rotated.").pack()
 
 	def ENTER_event(self, event): # Rename the target photo when the enter key is pressed.
-		# shutil.move(input, output + str(user_input))
-		self.display_photo()
+		# TODO save rotated image on enter
+		# TODO crop rotated image to standardized size
+		
+		self.next_photo_viewer()
 		print(self.name_entry.get())
 		print("You hit return.")
 
 if __name__ == "__main__":
 	root = Tk()
 	MainApplication(root)
-
-	# LET THE USER PICK AN ANGLE, THEN JUST USE IMAGEMAGIC
 	mainloop()
