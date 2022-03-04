59f0065f34dbf1aca1e4ed9533138b886582e5ca
CWeaver <carsonweaver@outlook.com>
2022-03-04 11:19:43 -0500

    Added support for file organization

--- utils/resize_images.bat
+++ utils/resize_images.bat
@@ -1,13 +1,21 @@
-:: Grab pixel dimensions.
-SET /A p = %1
+:: Grab pixel dimensions, source, and destination paths.
+SET /A pixels = %1
+SET source=%2
+SET destination=%3
 
-:: Resize everything.
-cd ../data/revision_2
+echo %destination%, %source%
 
-:: Resize everything.
-magick mogrify -resize %p%x%p% *.png
+:: Duplicate the original test set.
+:: /y - Suppresses prompting to confirm that you want to overwrite an existing destination file.
+:: /i - Creates directory if it doesn't exist.
+:: /q - Suppresses the display of xcopy messages.
+xcopy /y /i /q %source% %destination%
+
+:: Jump to the copied directory.
+cd %destination%
+
+:: Shrink every image to fit in a X by X box.
+magick mogrify -resize %pixels%x%pixels% *.png
 
 :: Extent everything out to a single SQUARE size.
-magick mogrify -background black -gravity center -extent %p%x%p% *.png
-
-cd ../../scripts
+magick mogrify -background black -gravity center -extent %pixels%x%pixels% *.png
